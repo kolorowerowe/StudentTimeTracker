b24369b6d38f2cdf6d80aa72f3f4a4d56c9e8d01
Marcin Kozak <marcinkozak005@gmail.com>
2020-05-20 16:32:00 +0200

    Basic chronometer

--- app/src/main/java/com/github/studenttimetracker/fragments/TrackTimeFragment.java
+++ app/src/main/java/com/github/studenttimetracker/fragments/TrackTimeFragment.java
@@ -1,10 +1,12 @@
 package com.github.studenttimetracker.fragments;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,9 +15,12 @@ import androidx.fragment.app.Fragment;
 
 import com.github.studenttimetracker.R;
 
+import org.w3c.dom.Text;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class TrackTimeFragment extends Fragment {
     @Override
@@ -28,46 +33,51 @@ public class TrackTimeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_tracktime, container, false);
 
-        Button startButton = view.findViewById(R.id.startActivity);
-        Button endButton = view.findViewById(R.id.endActivity);
+        final Button startButton = view.findViewById(R.id.startActivity);
+        final Button endButton = view.findViewById(R.id.endActivity);
+        final TextView activityNameInput = view.findViewById(R.id.activityNameInput);
+        final Chronometer chronometer = view.findViewById(R.id.myChronometer);
+        final TextView spentTime = view.findViewById(R.id.spentTime);
+        final TextView activityNameShow = view.findViewById(R.id.activityNameShow);
+
         endButton.setVisibility(View.INVISIBLE);
+        chronometer.setVisibility(View.INVISIBLE);
+        activityNameShow.setVisibility(View.INVISIBLE);
+        spentTime.setVisibility(View.INVISIBLE);
 
         // Setting onClicks()
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                View parentView = (View) v.getParent();
+                chronometer.setBase(SystemClock.elapsedRealtime());
+                chronometer.start();
 
-                TextView startDateShow = parentView.findViewById(R.id.startTime);
-                Date date = new Date();
-                DateFormat dateFormat = new SimpleDateFormat("HH:MM");
-                startDateShow.setText(dateFormat.format(date));
+                activityNameShow.setText(activityNameInput.getText());
 
-                TextView activityName = parentView.findViewById(R.id.activityName);
-                activityName.setVisibility(View.INVISIBLE);
-                TextView activityNameShow = parentView.findViewById(R.id.activityNameShow);
-                activityNameShow.setText(activityName.getText());
-                v.setVisibility(View.INVISIBLE);
-                Button second = parentView.findViewById(R.id.endActivity);
-                second.setVisibility(View.VISIBLE);
+                endButton.setVisibility(View.VISIBLE);
+                chronometer.setVisibility(View.VISIBLE);
+                activityNameInput.setVisibility(View.INVISIBLE);
+                startButton.setVisibility(View.INVISIBLE);
             }
         });
 
         endButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                View parentView = (View) v.getParent();
+                long duration = (SystemClock.elapsedRealtime() - chronometer.getBase());
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+                simpleDateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
 
-                TextView endDateShow = parentView.findViewById(R.id.endTime);
-                Date date = new Date();
-                DateFormat dateFormat = new SimpleDateFormat("HH:MM");
-                endDateShow.setText(dateFormat.format(date));
+                spentTime.setText(simpleDateFormat.format(duration));
+                chronometer.stop();
+                chronometer.setBase(SystemClock.elapsedRealtime());
 
-                TextView activityName = parentView.findViewById(R.id.activityName);
-                activityName.setVisibility(View.VISIBLE);
-                Button startActivity = parentView.findViewById(R.id.startActivity);
-                startActivity.setVisibility(View.VISIBLE);
-                v.setVisibility(View.INVISIBLE);
+                spentTime.setVisibility(View.VISIBLE);
+                activityNameShow.setVisibility(View.VISIBLE);
+                activityNameInput.setVisibility(View.VISIBLE);
+                startButton.setVisibility(View.VISIBLE);
+                endButton.setVisibility(View.INVISIBLE);
+                chronometer.setVisibility(View.INVISIBLE);
 
             }
         });
