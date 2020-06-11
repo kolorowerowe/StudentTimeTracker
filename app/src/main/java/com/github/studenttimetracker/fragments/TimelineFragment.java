2422b18214d9b4c685cb0b3c1ad499a88c8408aa
Marcin Kozak <marcinkozak005@gmail.com>
2020-06-11 16:14:18 +0200

    add Quotes REST API to TimelineFragment

--- app/src/main/java/com/github/studenttimetracker/fragments/TimelineFragment.java
+++ app/src/main/java/com/github/studenttimetracker/fragments/TimelineFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,11 +12,21 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.github.studenttimetracker.R;
 import com.github.studenttimetracker.database.Repository;
 import com.github.studenttimetracker.models.Task;
 import com.github.studenttimetracker.recycleView.TimeEntryAdapter;
 import com.github.studenttimetracker.utils.CalendarUtils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
@@ -28,6 +39,10 @@ public class TimelineFragment extends Fragment {
     private Repository repository;
     private List<Task> timeEntryList = new ArrayList<>();
     private TimeEntryAdapter timeEntryAdapter;
+    private RequestQueue requestQueue;
+    private static final String API_URL_QOD = "https://quotes.rest/qod";
+    private TextView quoteText;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -49,6 +64,11 @@ public class TimelineFragment extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         recyclerView.setAdapter(timeEntryAdapter);
 
+        // REST
+        quoteText = view.findViewById(R.id.quoteText);
+        requestQueue = Volley.newRequestQueue(requireContext());
+        parseJSON();
+
         return view;
     }
 
@@ -65,6 +85,37 @@ public class TimelineFragment extends Fragment {
         assert taskList != null;
         timeEntryList.addAll(taskList);
         return timeEntryList;
+    }
+
+    private void parseJSON() {
+        final JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.GET, API_URL_QOD, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONObject content = response.getJSONObject("contents");
+                            JSONArray quotesArray = content.getJSONArray("quotes");
+                            JSONObject quoteObject = quotesArray.getJSONObject(0);
+
+                            String quote = quoteObject.getString("quote");
+                            String author = quoteObject.getString("author");
+                            quoteText.append("Quote of the day:\n"+quote+"\n~"+author);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                        quoteText.clearComposingText();
+                        quoteText.append("Enable internet connection to see motivational quotes!");
+                    }
+                });
+        requestQueue.add(request);
     }
 
 }
